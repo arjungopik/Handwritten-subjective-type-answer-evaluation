24a230543f15282facfd37b1247a84c09bbd80df
arjungopik <arjungopik111@gmail.com>
2024-03-31 23:35:00 +0530

    Updation on answerkey

---
+++ main.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def execute_program1():
+    subprocess.run(["venv38\Scripts\python.exe", "app.py"], check=True)
+
+def execute_program2():
+    subprocess.run(["venv312\Scripts\python.exe", "gemini.py"], check=True)
+    subprocess.run(["venv312\Scripts\python.exe", "simcheck.py"], check=True)
+
+if __name__ == "__main__":
+    execute_program1()
+    execute_program2()
